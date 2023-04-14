85890249c77568becba05749568e70ba97ca763e
umaransar03 <umaransarmusic@gmail.com>
2023-04-13 21:53:14 -0400

    icons

--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -7,9 +7,19 @@ const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
-        fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, GeoApiOptions)
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, GeoApiOptions)
             .then(response => response.json())
-            .then(response => console.log(response))
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
             .catch(err => console.error(err));
     }
 
--- src/screens/api/API.js
+++ src/screens/api/API.js
@@ -1,9 +1,9 @@
 export const GeoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e46a28497emshbaec3c6edcb7115p1e32b6jsn85ff748df8a6',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e46a28497emshbaec3c6edcb7115p1e32b6jsn85ff748df8a6',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
